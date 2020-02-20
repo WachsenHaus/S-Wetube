f91e41e27926220696ab4dae041a38cffb64c776
WachsenHaus <59411545+WachsenHaus@users.noreply.github.com>
2020-02-20 15:02:59 +0900

    2020-02-20 commit

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-const express = require("express");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
-app.listen(4000);
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`Listenin on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hello from home");
+
+const handleProfile = (req, res) => res.send("dddddd");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("common"));
+
+const middleware = (req, res, next) => {
+  res.send("not happening");
+};
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(4000, handleListening);
